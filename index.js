29a373ab4738a0e8e8c9ecf2e0c4709b83481ae9
Nahgy <tobias.laufs@gmail.com>
2017-04-06 19:07:37 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const request = require("request");
 const fs = require("fs");
+
+var currentChangeID = '58753553-62100676-58184518-67663682-62827449';
+
 var body;
 
-request('http://www.pathofexile.com/api/public-stash-tabs', (error, response, body) => {
+request('http://www.pathofexile.com/api/public-stash-tabs?id=' + currentChangeID, (error, response, body) => {
     if (error) throw error;
     if (response.statusCode === 200) {
         const formatted = JSON.stringify(JSON.parse(body), null, 2);
